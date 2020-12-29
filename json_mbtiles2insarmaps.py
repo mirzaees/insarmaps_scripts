378331958ed57f874c4851fa80a766da5c3fbe8a
stackTom <rtiky23@gmail.com>
2020-12-29 04:46:17 -0500

    dont print success if error

--- json_mbtiles2insarmaps.py
+++ json_mbtiles2insarmaps.py
@@ -188,10 +188,11 @@ def main():
             dbController.connect()
             try:
                 dbController.remove_dataset_if_there(parseArgs.remove)
+                dbController.remove_mbtiles(parseArgs.remove + ".mbtiles")
             except Exception as e:
                 print(str(e))
+
             dbController.close()
-            dbController.remove_mbtiles(parseArgs.remove + ".mbtiles")
             print("Successfully removed %s" % (parseArgs.remove))
 
 
